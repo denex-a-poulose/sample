a543ab8fa122fbe50c846617724cea6f6290c2ed
Denex A Poulose <107526656+denex-a-poulose@users.noreply.github.com>
2024-05-29 22:54:05 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.text').forEach(item => {
-    item.addEventListener('click', function() {
-        document.querySelector('.container').classList.add('slide-out');
-        setTimeout(function() {
-            window.location.href = item.getAttribute('data-url');
-        }, 1000); 
-    });
-});
-
